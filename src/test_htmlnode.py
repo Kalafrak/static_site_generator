4045de5af574b98d7d84115e5060efde3468f6b7
Kalafrak <mdelldeadstar@hotmail.co.uk>
2026-02-07 15:21:46 +0000

    add LeafNode child class to htmlnode.py along with tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_none(self):
@@ -18,6 +19,26 @@ class TestHTMLNode(unittest.TestCase):
         }
         node = HTMLNode("a", "Google", None, props)
         self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_raw_text(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(node.to_html(), "Just text")
+
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("a", "Click me", {"href": "https://www.google.com"})
+        self.assertEqual(
+        node.to_html(),
+        '<a href="https://www.google.com">Click me</a>',
+        )
+
+    def test_leaf_to_html_raises_without_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
 
 
 if __name__ == "__main__":
