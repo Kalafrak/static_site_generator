ef0d505f5e692ca039ae9c1c056beb7846c88255
Kalafrak <mdelldeadstar@hotmail.co.uk>
2026-02-21 16:04:46 +0000

    add split_nodes_image() UNFINISHED

--- src/textnode.py
+++ src/textnode.py
@@ -62,3 +62,29 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            image_tuples = extract_markdown_images(node.text)
+            if image_tuples == []:
+                new_nodes.append(node)
+            else:
+                remaining_text = node.text
+                for image_tuple in image_tuples:
+                    alt_text = image_tuple[0]
+                    url = image_tuple[1]
+                    sections = remaining_text.split(f"![{alt_text}]({url})", 1)
+                    remaining_text = sections[1]
+                    if sections[0] != "":
+                        new_nodes.append(TextNode(sections[0], TextType.TEXT))
+                    new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
+                if remaining_text != "":
+                    new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+    return new_nodes
+
+
+def split_nodes_links(old_nodes):
