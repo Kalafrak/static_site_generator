173d320fc38bf2fea38922ba94a48be0e0f8d4b6
Kalafrak <mdelldeadstar@hotmail.co.uk>
2026-04-27 19:48:05 +0100

    began writing generate_page()

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,3 +1,6 @@
+from markdown_blocks import markdown_to_html_node
+from htmlnode import ParentNode
+
 def extract_title(markdown):
     lines = markdown.split("\n")
     for line in lines:
@@ -6,3 +9,13 @@ def extract_title(markdown):
             stripped_line = stripped_prefix.strip()
             return stripped_line
     raise Exception("No title found!")
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
+    with open(from_path) as f:
+        from_file = f.read()
+    with open(template_path) as t:
+        template_file = t.read()
+    from_html_node = markdown_to_html_node(from_file)
+    from_html_string = from_html_node.to_html()
+
